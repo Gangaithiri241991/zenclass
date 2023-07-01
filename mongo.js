21e631413d2f7870d34c5ab11710950e51232b31
Gangaithiri241991 <ajimansamy29@gmail.com>
2023-07-01 18:13:03 +0530

    first commit

---
+++ mongo.js
@@ -0,0 +1,599 @@
+db.students.insertMany([
+    {
+      "name":"prasanth",
+      "batch":"B45WD",
+      "qualification":"B.E",
+      "experience" : 2,
+      "taskCompletion":75,
+      "gender":"male"
+    },
+    {
+      "name":"Ajay",
+      "batch":"B42WD",
+      "qualification":"BCA",
+      "experience" : 5,
+      "taskCompletion":80,
+      "gender":"male"
+    },
+    {
+      "name":"Abdur",
+      "batch":"B43WD",
+      "qualification":"BCA",
+      "experience" : 3,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Anitha",
+      "batch":"B40WD",
+      "qualification":"B.COM",
+      "experience" : 1,
+      "taskCompletion":45,
+      "gender":"female"
+    },
+    {
+      "name":"Anirutha",
+      "batch":"B41WD",
+      "qualification":"BSC",
+      "experience" : 4,
+      "taskCompletion":90,
+      "gender":"female"
+    },
+    {
+      "name":"sivakumar",
+      "batch":"B45WD",
+      "qualification":"B.E",
+      "experience" : 2,
+      "taskCompletion":65,
+      "gender":"male"
+    },
+    {
+      "name":"Aswin",
+      "batch":"B42WD",
+      "qualification":"BCA",
+      "experience" : 5,
+      "taskCompletion":80,
+      "gender":"male"
+    },
+    {
+      "name":"Balaji",
+      "batch":"B43WD",
+      "qualification":"BCA",
+      "experience" : 3,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Vijipriya",
+      "batch":"B40WD",
+      "qualification":"B.COM",
+      "experience" : 1,
+      "taskCompletion":45,
+      "gender":"female"
+    },
+    {
+      "name":"Sharmila",
+      "batch":"B41WD",
+      "qualification":"BSC",
+      "experience" : 4,
+      "taskCompletion":90,
+      "gender":"female"
+    },
+    {
+      "name":"Venkat",
+      "batch":"B45WD",
+      "qualification":"B.E",
+      "experience" : 2,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Jeeva",
+      "batch":"B42WD",
+      "qualification":"BCA",
+      "experience" : 5,
+      "taskCompletion":60,
+      "gender":"male"
+    },
+    {
+      "name":"Sathya",
+      "batch":"B43WD",
+      "qualification":"BCA",
+      "experience" : 4,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Anitha",
+      "batch":"B40WD",
+      "qualification":"B.COM",
+      "experience" : 1,
+      "taskCompletion":45,
+      "gender":"female"
+    },
+    {
+      "name":"Anirutha",
+      "batch":"B41WD",
+      "qualification":"BSC",
+      "experience" : 4,
+      "taskCompletion":90,
+      "gender":"female"
+    },
+    {
+      "name":"Vimal",
+      "batch":"B45WD",
+      "qualification":"B.E",
+      "experience" : 2,
+      "taskCompletion":75,
+      "gender":"male"
+    },
+    {
+      "name":"Nalin",
+      "batch":"B42WD",
+      "qualification":"BCA",
+      "experience" : 5,
+      "taskCompletion":80,
+      "gender":"male"
+    },
+    {
+      "name":"Navin",
+      "batch":"B43WD",
+      "qualification":"BCA",
+      "experience" : 3,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Radhika",
+      "batch":"B40WD",
+      "qualification":"B.COM",
+      "experience" : 1,
+      "taskCompletion":45,
+      "gender":"female"
+    },
+    {
+      "name":"Ramya",
+      "batch":"B41WD",
+      "qualification":"BSC",
+      "experience" : 4,
+      "taskCompletion":90,
+      "gender":"female"
+    },
+    {
+      "name":"Jeeva",
+      "batch":"B45WD",
+      "qualification":"B.E",
+      "experience" : 2,
+      "taskCompletion":75,
+      "gender":"male"
+    },
+    {
+      "name":"srider",
+      "batch":"B42WD",
+      "qualification":"BCA",
+      "experience" : 5,
+      "taskCompletion":80,
+      "gender":"male"
+    },
+    {
+      "name":"Sathya",
+      "batch":"B43WD",
+      "qualification":"BCA",
+      "experience" : 3,
+      "taskCompletion":55,
+      "gender":"male"
+    },
+    {
+      "name":"Anitha Kaladharan",
+      "batch":"B40WD",
+      "qualification":"B.COM",
+      "experience" : 1,
+      "taskCompletion":45,
+      "gender":"female"
+    },
+    {
+      "name":"Aswini",
+      "batch":"B41WD",
+      "qualification":"BSC",
+      "experience" : 4,
+      "taskCompletion":90,
+      "gender":"female"
+    },
+    ])
+
+
+    db.mentor.insertMany([{
+
+        "name":"vishnu",
+        "role":"mentor",
+        "batch":"b44wtamil",
+        "experience":"12"
+    },
+    {
+    
+        "name":"anitha",
+        "role":"mentor fsd",
+        "batch":"b45wtamil",
+        "experience":"1"
+    },
+    
+    {
+    
+        "name":"sanjay",
+        "role":"mentor fsd",
+        "batch":"b46wtamil",
+        "experience": "10"
+    },
+    {
+    
+        "name":"arun",
+        "role":"mentor fsd",
+        "batch":"b47wtamil",
+        "experience":"15"
+    },
+    {
+    
+        "name":"vishnu raj",
+        "role":"mentor",
+        "batch":"b48wtamil",
+        "experience":"15"
+    },
+    
+    ])
+    db.company_drives.insertMany([
+        {
+            "company_drives":"hcl",
+            "name":"anitha",
+            "month":"oct-15-2020",
+            "batch":"b48wtamil",
+
+        },
+        {
+            "company_drives":"hcl",
+            "name":"vijipriya",
+            "month":"oct-16-2020",
+            "batch":"b48wtamil",
+    
+        },
+        {
+        "company_drives" : "ibm",
+            "name":"karthik",
+            "month":"oct-15-2020",
+            "batch":"b48wtamil",
+        },{
+
+            "company_drives":"capegemin",
+            "name":"R kala",
+            "month":"oct-15-2020",
+            "batch":"b44wtamil",
+
+        },{
+        "company_drives":"hcl",
+            "name":"bala",
+            "month":"oct-15-2020",
+            "batch":"b44wtamil",
+        },
+{
+
+
+    "company_drives":"hcl",
+            "name":"arun",
+            "month":"oct-30-2020",
+            "batch":"b43wtamil",
+
+},
+{
+    "company_drives":"ibm",
+            "name":"rahul",
+            "month":"oct-31-2020",
+            "batch":"b45wtamil",
+
+}
+    ])
+
+    db.codekata.insertMany([{
+        "name":"anitha",
+        "completion":"25",
+        "batch":"b48wtamil",
+        "date":"oct-15-2020"
+
+
+    },
+    {
+    "name":"ajitha",
+    "completion":"20",
+    "batch":"b45wtamil",
+    "date":"0ct-15-2020",
+
+
+},
+
+
+    {
+        "name":"rahul",
+        "completion":"50",
+        "batch":"b4w5tamil",
+        "date":"0ct-31-2020",
+    
+    
+    },
+    
+    
+        {
+            "name":"mani",
+            "completion":"50",
+            "batch":"b43w5tamil",
+            "date":"0ct-30-2020",
+        
+        
+        },
+        
+        {
+            "name":"sella",
+            "completion":"50",
+            "batch":"b43w5tamil",
+            "date":"0ct-31-2020",
+        
+        
+        },
+        
+
+        {
+            "name":"rahul",
+            "completion":"50",
+            "batch":"b4w5tamil",
+            "date":"0ct-31-2020",
+        
+        
+        },
+        {
+            "name":"chithra",
+            "completion":"10",
+            "batch":"b4w5tamil",
+            "date":"0ct-18-2020",
+        
+        
+        },
+        {
+            "name":"mugi",
+            "completion":"0",
+            "batch":"b4w5tamil",
+            "date":"0ct-16-2020",
+        
+        
+        },
+        {
+            "name":"jeya",
+            "completion":"0",
+            "batch":"b4w5tamil",
+            "date":"0ct-15-2020",
+        
+        
+        },
+        {
+            "name":"lakshmi",
+            "completion":"0",
+            "batch":"b4w5tamil",
+            "date":"0ct-30-2020",
+        
+        
+        },
+        {
+            "name":"vino",
+            "completion":"25",
+            "batch":"b4w5tamil",
+            "date":"0ct-18-2020",
+        
+        
+        },
+        {
+            "name":"raj",
+            "completion":"45",
+            "batch":"b4w5tamil",
+            "date":"0ct-18-2020",
+        
+        
+        },
+        {
+            "name":"kala",
+            "completion":"50",
+            "batch":"b4w5tamil",
+            "date":"0ct-22-2020",
+        
+        
+        },
+                                    
+
+
+
+])
+
+db.user.insertMany([{
+    "name":"anitha",
+    "completion":"25",
+    "batch":"b48wtamil",
+    "month":"oct-15-2020",
+    "attendance":"present",
+    "task":"rest countries",
+    "topic":"javascript",
+
+},
+{
+"name":"ajitha",
+"completion":"20",
+"batch":"b45wtamil",
+"month":"0ct-27-2020",
+"attendance":"absent",
+"task":"userlogin",
+"topic":"postman,mongo",
+
+
+},
+
+
+{
+    "name":"rahul",
+    "completion":"50",
+    "batch":"b4w5tamil",
+    "month":"0ct-25-2020",
+    "attendance":"absent",
+    "task":"student attendence dashboard",
+    "topic":"nodejs,express,mongodb",
+
+},
+
+
+    {
+        "name":"mani",
+        "completion":"50",
+        "batch":"b43w5tamil",
+        "month":"0ct-21-2020",
+        "attendance":"absent",
+        "task":"create student database",
+        "topic":"mongoshell",
+    
+    },
+    
+    {
+        "name":"sella",
+        "completion":"50",
+        "batch":"b43w5tamil",
+        "month":"0ct-20-2020",
+        "attendance":"present",
+        "task":"sql",
+        "topic":"sql queries",
+    
+    },
+    
+
+    {
+        "name":"rahul",
+        "completion":"50",
+        "batch":"b4w5tamil",
+        "month":"0ct-19-2020",
+        "attendance":"absent",
+        "task":"mongodb connection",
+        "topic":"mongodb",
+    
+    
+    },
+    {
+        "name":"chithra",
+        "completion":"0",
+        "batch":"b4w5tamil",
+        "month":"0ct-18-2020",
+        "attendance":"absent",
+        "task":"react",
+        "topic":"rest API",
+    
+    
+    },
+    {
+        "name":"mugi",
+        "completion":"0",
+        "batch":"b4w5tamil",
+        "month":"0ct-16-2020",
+        "attendance":"absent",
+        "task":"express connection",
+        "topic":"backend",
+    
+    
+    },
+    {
+        "name":"jeya",
+        "completion":"0",
+        "batch":"b4w5tamil",
+        "month":"0ct-15-2020",
+        "attendance":"absent",
+        "task":"library",
+        "topic":"react",
+    
+    
+    },
+    {
+        "name":"lakshmi",
+        "completion":"10",
+        "batch":"b4w5tamil",
+        "month":"0ct-30-2020",
+        "attendance":"absent",
+        "task":"callback",
+        "topic":"javascript",
+    
+    },
+    {
+        "name":"vino",
+        "completion":"25",
+        "batch":"b4w5tamil",
+        "month":"0ct-18-2020",
+        "attendance":"absent",
+        "task":"redux",
+        "topic":"student dashboard",
+    
+    },
+    {
+        "name":"raj",
+        "completion":"45",
+        "batch":"b4w5tamil",
+        "month":"0ct-19-2020",
+        "attendance":"present",
+        "task":"react",
+        "topic":"react hook",
+    
+    
+    },
+    {
+        "name":"kala",
+        "completion":"50",
+        "batch":"b4w5tamil",
+        "month":"0ct-22-2020",
+        "attendance":"absent",
+        "task":"promise asyn",
+        "topic":"asynchronus call",
+    
+    },
+                                
+
+
+
+])
+
+//1. Find all the topics and tasks which are thought in the month of Octoberdb.students.find({}).toArray()
+db.user.find().forEach(function(use){("topic: ","month: october   ")})
+
+//2.Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+db.company_drives.find()
+{createdAt:{$gte:ISODate('2020-3-1');$lt:ISODate("2021-4-1")}}
+//3.Find all the company drives and students who are appeared for the placement.
+db.company_drives.find().forEach(function(comp){print(comp.name,comp.batch)})
+// 4.Find the number of problems solved by the user in codekata
+db.users.aggregate([
+    {
+      $match: {
+        username: "ajitha"
+      }
+    },
+    {
+      $project: {
+        numberOfProblemsSolved: { $size: "$problemsSolved" }
+      }
+    }
+  ])
+  
+//5 Find all the mentors with who has the mentee's count more than 15
+db.mentors.find({
+    $expr: {
+      $gt: [{ $size: "$mentees" }, 15]
+    }
+  },
+)
+
+//6. Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+
+db.users.count({
+    $and: [
+      { date: { $gte: ISODate("2020-10-15T00:00:00Z"), $lte: ISODate("2020-10-31T23:59:59Z") } },
+      { status: "absent" },
+      { taskSubmitted: { $ne: true } }
+    ]
+  })
+  
